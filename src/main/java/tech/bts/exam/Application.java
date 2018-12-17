6c482b57b7c04197b335b9c8f1385c09db03e9b1
Alessandro <alessandro.zaccolo@bts.tech>
2018-12-17 10:55:35 +0100

    Exercise 1

--- src/main/java/tech/bts/exam/Application.java
+++ src/main/java/tech/bts/exam/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import tech.bts.exam.service.CounterService;
 
 
 @SpringBootApplication
@@ -14,10 +15,10 @@ public class Application {
 
 
     @Bean
-    public CommandLineRunner createDummyData(){
+    public CommandLineRunner createDummyData(CounterService counterService){
         return args -> {
 
-
+            counterService.getCounter();
 
         };
     }
